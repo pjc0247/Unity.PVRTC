94e7c9725a3afc3b944ab532389e3928697b8e94
Park June Chul <pjc0247@naver.com>
2016-08-10 14:04:06 +0900

    Update RPVRTC.cs

--- RPVRTC.cs
+++ RPVRTC.cs
@@ -4,32 +4,50 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 
-public class RPVRTC {
+public class RPVRTC
+{
+    private static CoProvider obj { get; set; }
+    private class CoProvider : MonoBehaviour
+    {
+        void Start()
+        {
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+        public void PublicStartCoroutine(IEnumerator co)
+        {
+            StartCoroutine(co);
+        }
+    }
 
-  public static SignalEnumerator<Texture2D> Compress24(Texture2D tex)
-  {
-      var signal = new SignalEnumerator<Texture2D>();
-      StartCoroutine(_Compress24(tex, signal));
-      return signal;
-  }
-  private static IEnumerator _Compress24(Texture2D tex, SignalEnumerator<Texture2D> signal)
-  {
-      var width = tex.width;
-      var height = tex.height;
-      var resultTexture = new Texture2D(width, height, TextureFormat.PVRTC_RGB4, false, true);
-      byte[] compressed = null;
-      Color[] pixels = tex.GetPixels();
-  
-      yield return new AsyncExecutor<object>(x =>
-      {
-          PvrtcCompress compressor = new PvrtcCompress();
-          compressed = compressor.EncodeRgb4Bpp(pixels, width, height);
-      });
-  
-      resultTexture.LoadRawTextureData(compressed);
-      resultTexture.Apply();
-      
-      signal.Notify(resultTexture);
-  }
+    public static SignalEnumerator<Texture2D> Compress24(Texture2D tex)
+    {
+        if (obj == null)
+        {
+            var go = new GameObject("RPVRTC");
+            obj = go.AddComponent<CoProvider>();
+        }
 
+        var signal = new SignalEnumerator<Texture2D>();
+        obj.StartCoroutine(_Compress24(tex, signal));
+        return signal;
+    }
+    private static IEnumerator _Compress24(Texture2D tex, SignalEnumerator<Texture2D> signal)
+    {
+        var width = tex.width;
+        var height = tex.height;
+        var resultTexture = new Texture2D(width, height, TextureFormat.PVRTC_RGB4, false, true);
+        byte[] compressed = null;
+        Color[] pixels = tex.GetPixels();
+
+        yield return new AsyncExecutor<object>(x =>
+        {
+            PvrtcCompress compressor = new PvrtcCompress();
+            compressed = compressor.EncodeRgb4Bpp(pixels, width, height);
+        });
+
+        resultTexture.LoadRawTextureData(compressed);
+        resultTexture.Apply();
+
+        signal.Notify(resultTexture);
+    }
 }
